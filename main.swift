723423ab7b661e8e6d71fabec44e0e2e0b73369b
DarkValerion18 <211044311+DarkVelarion18@users.noreply.github.com>
2025-08-06 18:37:54 +0200

    Implement basic GameScene with score variable and automatic label update

--- main.swift
+++ main.swift
@@ -1,3 +1,15 @@
 import SpriteKit
 import GameplayKit
+import Foundation
+import GameKit
 
+class GameScene: SKScene {
+
+    var player: SKScene!
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+}
